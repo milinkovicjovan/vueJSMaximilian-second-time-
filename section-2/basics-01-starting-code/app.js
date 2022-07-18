afd573801104a62417fd8e88b1737df6cf86ae89
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-18 22:11:07 +0200

    vue.js_course

---
+++ section-2/basics-01-starting-code/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master Vue and build amazing apps!',
+      vueLink: 'https://vuejs.org/',
+    };
+  },
+  // data always returns an object and always an object
+  // data contain key values
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
+  },
+});
+
+app.mount('#user-goal');
