1fa78934b4905f89562866c36d843fd27ee6d3c1
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-19 23:17:17 +0200

    vue.js_course

--- section-2/basics-05-using-the-native-event-object/app.js
+++ section-2/basics-05-using-the-native-event-object/app.js
@@ -3,15 +3,40 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      lastName: '',
+      // fullname: '',
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // },
   },
   computed: {
     fullname() {
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      // we add lastname if the name is not empty
-      return this.name + ' ' + 'Milinkovic';
+      return this.name + ' ' + this.lastName;
     },
   },
   methods: {
