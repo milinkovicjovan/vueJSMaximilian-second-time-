4ef618128fdef22d663bc2091fbb9d3b523d7800
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-21 21:25:57 +0200

    vue.js_course

---
+++ section-2/basics-assignment-3-problem/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      number: 0,
+    };
+  },
+  watch: {
+    result() {
+      console.log('Watcher executing...');
+      const that = this;
+      setTimeout(function () {
+        that.number = 0;
+      }, 5000);
+    },
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return 'Too much';
+      }
+    },
+  },
+  methods: {
+    addNumber(num) {
+      this.number = this.number + num;
+    },
+  },
+});
+
+app.mount('#assignment');
