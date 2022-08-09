d441900926b07f476d21361681d02e18508f93dc
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-09 21:18:25 +0200

    creating-basic-app

---
+++ section-7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0123 99978 90",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
