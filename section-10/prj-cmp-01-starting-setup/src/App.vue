d89a9700f6231946f8d79268824186f2a4f4ed0f
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-13 22:31:53 +0200

    firstAPP

--- section-10/prj-cmp-01-starting-setup/src/App.vue
+++ section-10/prj-cmp-01-starting-setup/src/App.vue
@@ -1,45 +1,27 @@
 <template>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 export default {
   components: {
-    StoredResources,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google-guide',
-          title: 'Google Guide',
-          description: 'Learn to google...',
-          link: 'https://google.com',
-        },
-      ],
-    };
+    TheHeader,
+    TheResources,
   },
 };
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
 * {
   box-sizing: border-box;
 }
-
 html {
   font-family: 'Roboto', sans-serif;
 }
-
 body {
   margin: 0;
 }
