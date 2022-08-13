548d1dd5be18db663c61f492071830bcef7dcb40
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-13 20:22:00 +0200

    courseProjectLearningResourcesApp

---
+++ section-10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google-guide',
+          title: 'Google Guide',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
