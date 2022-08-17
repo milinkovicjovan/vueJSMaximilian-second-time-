474469b75b56fcf91698c363efc7fdfe086abd3e
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-17 18:21:22 +0200

    redirecting

--- section-13/routing-01-starting-setup/src/main.js
+++ section-13/routing-01-starting-setup/src/main.js
@@ -4,12 +4,15 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList }, // our-domain.com/teams => UsersList
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
