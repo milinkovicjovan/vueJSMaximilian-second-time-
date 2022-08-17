2c03b8ef6a8d736df99006699003577cff09c101
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-17 16:42:30 +0200

    passingDataWithRouteParams

--- section-13/routing-01-starting-setup/src/main.js
+++ section-13/routing-01-starting-setup/src/main.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList }, // our-domain.com/teams => UsersList
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   linkActiveClass: 'active',
 });
