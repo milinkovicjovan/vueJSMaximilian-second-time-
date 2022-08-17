20f1c6633f8d14a50a4deaa499e815f27d6edc37
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-17 20:22:16 +0200

    renderingMultipleRoutes

--- section-13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
+++ section-13/routing-01-starting-setup/src/components/teams/TeamsItem.vue
@@ -17,6 +17,7 @@ export default {
         params: {
           teamId: this.id,
         },
+        query: { sort: 'asc' },
       };
       // this.$router.push({name: 'team-members', params: {teamId: this.id}})
     },
--- section-13/routing-01-starting-setup/src/main.js
+++ section-13/routing-01-starting-setup/src/main.js
@@ -5,6 +5,9 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -12,7 +15,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
           name: 'team-members',
@@ -22,7 +25,13 @@ const router = createRouter({
         },
       ],
     }, // our-domain.com/teams => TeamsList
-    { path: '/users', component: UsersList }, // our-domain.com/teams => UsersList
+    {
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
+    }, // our-domain.com/teams => UsersList
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
