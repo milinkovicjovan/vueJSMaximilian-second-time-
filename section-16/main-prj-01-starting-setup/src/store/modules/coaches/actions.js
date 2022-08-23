12c0efaedc2e377baa3fb8c9b4b3eedb0b397574
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-23 12:38:24 +0200

    fetchingCoachDataGETRequest

--- section-16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ section-16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -28,4 +28,29 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://main-project-find-a-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // ...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
