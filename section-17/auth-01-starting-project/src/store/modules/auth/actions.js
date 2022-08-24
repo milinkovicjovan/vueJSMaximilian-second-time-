98883017be4b0fd57ef0a1b750db01be1ae10c12
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-08-24 21:26:53 +0200

    preparingVuexAndActionAndFlow

---
+++ section-17/auth-01-starting-project/src/store/modules/auth/actions.js
@@ -0,0 +1,30 @@
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBDnxR61P7E3U3C7_uTq2JNn6AbA5R_t9I',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'Failed to authenticate');
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
